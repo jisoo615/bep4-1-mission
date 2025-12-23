572a8046441cafcdf2ad05902cb055afd0948f69
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-23 12:31:40 +0900

    0003 댓글 8개

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -2,10 +2,11 @@ package com.back.entity;
 
 
 import com.back.jpa.entity.BaseIdAndTime;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
 
@@ -18,9 +19,24 @@ public class Post extends BaseIdAndTime {
     @Column(columnDefinition = "LONGTEXT")
     private String content;
 
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    List<PostComment> comments = new ArrayList<>();
+
     public Post(Member author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }
--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -30,6 +30,7 @@ public class DataInit {// 빌드가 다 되고 처음에 시작하는 클래스�
         return args -> {
             self.makeBaseMembers();
             self.makeBasePosts();
+            self.makeBaseComments();
         };
     }
 
@@ -58,5 +59,33 @@ public class DataInit {// 빌드가 다 되고 처음에 시작하는 클래스�
         Post post4 = postService.write(user2Member, "제목4", "내용4");
         Post post5 = postService.write(user2Member, "제목5", "내용5");
         Post post6 = postService.write(user3Member, "제목6", "내용6");
+    }
+
+    @Transactional
+    public void makeBaseComments(){
+        Post post1 = postService.findById(1).get();
+        Post post2 = postService.findById(2).get();
+        Post post3 = postService.findById(3).get();
+        Post post4 = postService.findById(4).get();
+        Post post5 = postService.findById(5).get();
+        Post post6 = postService.findById(6).get();
+
+        Member user1Member = memberService.findByUsername("user1").get();
+        Member user2Member = memberService.findByUsername("user2").get();
+        Member user3Member = memberService.findByUsername("user3").get();
+
+        if (post1.hasComments()) return;
+
+        post1.addComment(user1Member, "댓글1");
+        post1.addComment(user2Member, "댓글2");
+        post1.addComment(user3Member, "댓글3");
+
+        post2.addComment(user2Member, "댓글4");
+        post2.addComment(user2Member, "댓글5");
+
+        post3.addComment(user3Member, "댓글6");
+        post3.addComment(user3Member, "댓글7");
+
+        post4.addComment(user1Member, "댓글8");
     }
 }
