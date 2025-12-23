43f6595d18557d2454547446767a449cc2dcac30
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-23 12:13:13 +0900

    0002

--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -1,7 +1,10 @@
 package com.back.initData;
 
 import com.back.entity.Member;
+import com.back.entity.Post;
+import com.back.repository.PostRepository;
 import com.back.service.MemberService;
+import com.back.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,16 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class DataInit {// 빌드가 다 되고 처음에 시작하는 클래스임.
     private final DataInit self;
     private final MemberService memberService;
+    private final PostService postService;
 
-    public DataInit(@Lazy DataInit self, MemberService memberService) {
+    public DataInit(@Lazy DataInit self, MemberService memberService, PostService postService) {
         this.self = self;
         this.memberService = memberService;
+        this.postService = postService;
     }
 
     @Bean
     public ApplicationRunner baseInitDataRunner() {
         return args -> {
             self.makeBaseMembers();
+            self.makeBasePosts();
         };
     }
 
@@ -37,5 +43,20 @@ public class DataInit {// 빌드가 다 되고 처음에 시작하는 클래스�
         Member user1Member = memberService.join("user1", "1234", "유저1");
         Member user2Member = memberService.join("user2", "1234", "유저2");
         Member user3Member = memberService.join("user3", "1234", "유저3");
+    }
+
+    @Transactional
+    public void makeBasePosts(){
+        if(postService.count() >0) return;
+        Member user1Member = memberService.findByUsername("user1").get();
+        Member user2Member = memberService.findByUsername("user2").get();
+        Member user3Member = memberService.findByUsername("user3").get();
+
+        Post post1 = postService.write(user1Member, "제목1", "내용1");
+        Post post2 = postService.write(user1Member, "제목2", "내용2");
+        Post post3 = postService.write(user1Member, "제목3", "내용3");
+        Post post4 = postService.write(user2Member, "제목4", "내용4");
+        Post post5 = postService.write(user2Member, "제목5", "내용5");
+        Post post6 = postService.write(user3Member, "제목6", "내용6");
     }
 }
