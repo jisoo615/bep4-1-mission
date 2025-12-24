885eb70ace1977dab8e402d8b669dc7e027ba176
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-24 15:53:41 +0900

    0017 BaseMember 클래스 도입 - 원본, 복제 member

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.post.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
 import com.back.global.jpa.entity.BaseIdAndTimeManual;
+import com.back.shared.member.domain.ReplicaMember;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,10 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @Table(name = "POST_MEMBER")
-public class PostMember extends BaseIdAndTimeManual {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+public class PostMember extends ReplicaMember {
+    public PostMember(String username, String password, String nickname){
+        super(username, password, nickname);
+    }
 }
