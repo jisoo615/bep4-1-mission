9170f66305774d54acb7f95b1c6e4dfad3089f77
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-29 16:40:39 +0900

    0037 refactor 정책, base url

--- src/main/java/com/back/shared/post/out/PostApiClient.java
+++ src/main/java/com/back/shared/post/out/PostApiClient.java
@@ -1,6 +1,7 @@
 package com.back.shared.post.out;
 
 import com.back.shared.post.dto.PostDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -9,18 +10,22 @@ import java.util.List;
 
 @Service
 public class PostApiClient {
-    private static final RestClient restClient = RestClient.builder()
-            .baseUrl("http://localhost:8080/api/v1/post")
-            .build();
+    private final RestClient restClient;
 
-    public static List<PostDto> getItems() {
+    public PostApiClient(@Value("${custom.global.internalBackUrl}") String internalBackUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(internalBackUrl + "/api/v1/post")
+                .build();
+    }
+
+    public List<PostDto> getItems() {
         return restClient.get()
                 .uri("/posts")
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {});
     }
 
-    public static PostDto getItem() {
+    public PostDto getItem() {
         return restClient.get()
                 .uri("\"/posts/%d\".formatted(id)")
                 .retrieve()
