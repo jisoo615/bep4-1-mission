004f36eca3601dbed83b4ca8dbc4ed58e2d75253
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-29 14:34:00 +0900

    0035 주문 결제처리

--- src/main/java/com/back/boundedContext/cash/app/CashSupport.java
+++ src/main/java/com/back/boundedContext/cash/app/CashSupport.java
@@ -1,6 +1,7 @@
 package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.CashPolicy;
 import com.back.boundedContext.cash.domain.Wallet;
 import com.back.boundedContext.cash.out.CashMemberRepository;
 import com.back.boundedContext.cash.out.WalletRepository;
@@ -21,5 +22,17 @@ public class CashSupport {
 
     public Optional<Wallet> findWalletByHolder(CashMember holder) {
         return walletRepository.findByHolder(holder);
+    }
+
+    public Optional<Wallet> findWalletByHolderId(int holderId) {
+        return walletRepository.findByHolderId(holderId);
+    }
+
+    /**
+     * 구매자 지갑 -> 홀딩 -> 정산 -> 판매자
+     * 홀딩에 돈이 들어있다가 특정일에 정산해서 판매자에게 전달해줌
+     */
+    public Optional<Wallet> findHoldingWallet() {
+        return walletRepository.findByHolderId(CashPolicy.HOLDING_MEMBER_ID);
     }
 }
