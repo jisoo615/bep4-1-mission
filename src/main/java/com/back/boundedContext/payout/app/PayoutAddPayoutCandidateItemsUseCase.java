aa2f962f26dfc8e70db92a759ccad2c5e0942460
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-30 14:32:41 +0900

    0044 주문 품목 불러오기 후 PayoutCandidateItem 생성

--- src/main/java/com/back/boundedContext/payout/app/PayoutAddPayoutCandidateItemsUseCase.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutAddPayoutCandidateItemsUseCase.java
@@ -1,5 +1,9 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.PayoutCandidateItem;
+import com.back.boundedContext.payout.domain.PayoutEventType;
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutCandidateItemRepository;
 import com.back.shared.market.dto.OrderDto;
 import com.back.shared.market.dto.OrderItemDto;
 import com.back.shared.market.out.MarketApiClient;
@@ -7,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -14,13 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PayoutAddPayoutCandidateItemsUseCase {
     private final MarketApiClient marketApiClient;
+    private final PayoutSupport payoutSupport;
+    private final PayoutCandidateItemRepository payoutCandidateItemRepository;
+
 
     public void addPayoutCandidateItems(OrderDto order) {
-        List<OrderItemDto> items = marketApiClient.getOrderItems(order.getId());
+        marketApiClient.getOrderItems(order.getId())
+                        .forEach(orderItem -> makePayoutCandidateItems(order, orderItem));
 
-        items.forEach(item -> {
-            log.debug("orderItem.id : {}", item.getId());
-        });
     }
     /**
      * payout -> payoutCandyDateitems -> payoutItems
@@ -28,4 +34,58 @@ public class PayoutAddPayoutCandidateItemsUseCase {
      * 후보에 넣어두고 15일 후 구매확정 됐을 때 정산으로 보내는 것
      * 결제 완료 -> candidateItems에 추가 -> 15일 후 -> 정산
      */
+
+    /**
+     * candidate item을 2개 만드는 이유
+     * 판매자에게 결제금액-수수료, 홀딩(시스템)에게 수수료 이렇게 두 개를 만들어야 해서
+     */
+    private void makePayoutCandidateItems(
+            OrderDto order,
+            OrderItemDto orderItem
+    ) {
+        PayoutMember holding = payoutSupport.findHolingMember().get();
+        PayoutMember buyer = payoutSupport.findMemberById(orderItem.getBuyerId()).get();
+        PayoutMember seller = payoutSupport.findMemberById(orderItem.getSellerId()).get();
+
+        makePayoutCandidateItem(
+                PayoutEventType.정산__상품판매_수수료,
+                orderItem.getModelTypeCode(),
+                orderItem.getId(),
+                order.getPaymentDate(),
+                buyer,
+                holding, // 시스템에게 수수료
+                orderItem.getPayoutFee()
+        );
+
+        makePayoutCandidateItem(
+                PayoutEventType.정산__상품판매_대금,
+                orderItem.getModelTypeCode(),
+                orderItem.getId(),
+                order.getPaymentDate(),
+                buyer,
+                seller, // 판매자에게 대금
+                orderItem.getSalePriceWithoutFee()
+        );
+    }
+
+    private void makePayoutCandidateItem(
+            PayoutEventType eventType,
+            String relTypeCode,
+            int relId,
+            LocalDateTime paymentDate,
+            PayoutMember payer,
+            PayoutMember payee,
+            long amount
+    ) {
+        PayoutCandidateItem payoutCandidateItem = new PayoutCandidateItem(
+                eventType,
+                relTypeCode,
+                relId,
+                paymentDate,
+                payer,
+                payee,
+                amount
+        );
+        payoutCandidateItemRepository.save(payoutCandidateItem);
+    }
 }
